3c2d82dcddaf0c859242fb9a5e91b185444c968b
jeeves1618 <jeeves1618@gmail.com>
2024-02-01 18:42:22 +0530

    Added right and left swipe

--- js/books.js
+++ js/books.js
@@ -226,16 +226,60 @@ document
     enteredToken = enteredToken + event.key;
   });
 
-let element = document.querySelector("body");
-element.addEventListener("swipe", (event) => {
-  titleElement.textContent = "Swipe Event";
-});
-// swiped-left
-document.addEventListener("swiped-left", function (e) {
-  //alert("Go Left");
-});
-// swiped-right
-document.addEventListener("swiped-right", function (e) {
-  //alert("Go Right");
-});
-export { getTopics };
+let pageWidth = window.innerWidth || document.body.clientWidth;
+let treshold = Math.max(1, Math.floor(0.01 * pageWidth));
+let touchstartX = 0;
+let touchstartY = 0;
+let touchendX = 0;
+let touchendY = 0;
+
+const limit = Math.tan(((45 * 1.5) / 180) * Math.PI);
+const gestureZone = document.getElementById("modalContent");
+
+gestureZone.addEventListener(
+  "touchstart",
+  function (event) {
+    touchstartX = event.changedTouches[0].screenX;
+    touchstartY = event.changedTouches[0].screenY;
+  },
+  false
+);
+
+gestureZone.addEventListener(
+  "touchend",
+  function (event) {
+    touchendX = event.changedTouches[0].screenX;
+    touchendY = event.changedTouches[0].screenY;
+    handleGesture(event);
+  },
+  false
+);
+
+function handleGesture(e) {
+  let x = touchendX - touchstartX;
+  let y = touchendY - touchstartY;
+  let xy = Math.abs(x / y);
+  let yx = Math.abs(y / x);
+  if (Math.abs(x) > treshold || Math.abs(y) > treshold) {
+    if (yx <= limit) {
+      if (x < 0) {
+        let nextYear = Number(yearOfReading) + 1;
+        if (Number(yearOfReading) < currentYear)
+          document.querySelector("#next-year").click();
+        console.log("left");
+      } else {
+        document.querySelector("#prev-year").click();
+        console.log("right");
+      }
+    }
+    if (xy <= limit) {
+      if (y < 0) {
+        console.log("top");
+      } else {
+        console.log("bottom");
+      }
+    }
+  } else {
+    console.log("tap");
+  }
+}
